37068fc8364b14abc8699a5117b03b79afa77386
MANSOUR00DAHBI <code00net@gmail.com>
2025-01-29 22:01:28 +0100

    Added File H.h Header

---
+++ Header/H.h
@@ -0,0 +1,18 @@
+#ifndef H_H 
+#define H_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <signal.h>
+#include <assert.h>
+#include <float.h>
+#include <unistd.h> // Include this header for the usleep function
+#include <windows.h> // Include this header for the Windows 
+
+#endif // H_H						
