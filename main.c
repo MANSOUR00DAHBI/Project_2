4057d519439557284a7846d994e3d26507a80e88
MANSOUR00DAHBI <code00net@gmail.com>
2025-01-30 18:51:01 +0100

    modified main.c

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
+#include "INPUT/Read.c"
+#define R_H 
 
+extern int  nc, nw, nl;
 
 int main(){
-	printf("Hello World \n");
+
+	printf("Hello Press Key CTRL + D To Exit \n");
+	
+	IN_read();
+
+	printf("count : %3d Word : %3d line : %3d ", nc, nw , nl + 1 );
+
 	return 0;
 }
